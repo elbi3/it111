36ae3681f06db17c6227d1110f578e9c138a7c67
elbi3 <167570227+elbi3@users.noreply.github.com>
2025-05-02 10:27:08 -0700

    Update LitersToGallonsConverter.java
    Fixed output text

--- week4/LitersToGallonsConverter.java
+++ week4/LitersToGallonsConverter.java
@@ -5,8 +5,8 @@ public class LitersToGallonsConverter {
         double liters, gallons;
         int linesCounter = 0;
 
-        for (gallons = 1; gallons <= 100; gallons += 3){
-            
+        for (gallons = 1; gallons <= 100; gallons += 4){
+
             if(linesCounter % 5 == 0) {
                 System.out.println();
             }
@@ -16,9 +16,9 @@ public class LitersToGallonsConverter {
             liters = (gallons * 3.79);
 
             if(gallons == 1) {
-                System.out.println(gallons + " Gallon =  " + String.format("%.2f", liters) + " Liters");
+                System.out.println( String.format("%.2f", liters) + " Liters = " + gallons + " Gallon");
             } else {
-                System.out.println(gallons + " Gallons =  " + String.format("%.2f", liters) + " Liters");
+                System.out.println( String.format("%.2f", liters) + " Liters = " + gallons + " Gallons");
             }
         }
     }
