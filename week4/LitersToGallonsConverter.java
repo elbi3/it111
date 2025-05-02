1fe14d0cf09361d19a37199bc91ac98671d80e22
elbi3 <167570227+elbi3@users.noreply.github.com>
2025-05-02 10:09:53 -0700

    Update LitersToGallonsConverter.java
    Code refactor

--- week4/LitersToGallonsConverter.java
+++ week4/LitersToGallonsConverter.java
@@ -1,18 +1,12 @@
-import java.math.BigDecimal;
-import java.text.DecimalFormat;
-
 public class LitersToGallonsConverter {
 
     public static void main(String[] args) {
 
-        //ratio: 3.79 Liters = 1 Gallon
         double liters, gallons;
         int linesCounter = 0;
 
-
-        liters = 3.79;
-
         for (gallons = 1; gallons <= 100; gallons += 3){
+            
             if(linesCounter % 5 == 0) {
                 System.out.println();
             }
